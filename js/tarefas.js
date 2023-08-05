05cefdad8afb072fcee04d68e4c4c6152b9e363b
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-08-04 23:06:04 -0300

    teste3

--- js/tarefas.js
+++ js/tarefas.js
@@ -7,8 +7,35 @@ botaoAddTarefa.addEventListener("click", (evento)=>{
 
     const inputTarefa = document.querySelector("#idTarefa");
 
-    listaTarefasArray.push(inputTarefa.value)
+    listaTarefasArray.push(inputTarefa.value);
 
-    console.log(listaTarefasArray)
+    const listaTarefasUL = document.querySelector("#lista-tarefa");
+
+    let li = document.createElement("li");
+
+    li.textContent = inputTarefa.value
+
+    listaTarefasUL.appendChild(li);
+
+    let botaoApagaTarefa = document.createElement("button")
+
+    botaoApagaTarefa.textContent = " X ";
+
+    li.appendChild(botaoApagaTarefa);
+
+    botaoApagaTarefa.addEventListener("click", (e)=>{
+
+        e.target.parentNode.remove()
+
+        listaTarefasArray.forEach(tarefa=>{
+            if (tarefa == e.target.parentNode.textContent.split(" ")[0]){
+                listaTarefasArray.splice(listaTarefasArray.indexOf(tarefa),1)
+            }
+        });
+        
+        console.log(e.target.parentNode.textContent.split(" ")[0]);
+    })
+
+    console.log(listaTarefasArray); 
     inputTarefa.value = "";
 })
